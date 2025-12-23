1956d082e12f696f6c9443c461640d4969916191
Markus Reinhold <markusreinhold@icloud.com>
2025-12-23 13:50:08 +0100

    Add GetOpenGames (#13)
    Expose GetOpenGames and GetOpenGamesResponse.

--- go/connectfour/v1/gamingplatform.go
+++ go/connectfour/v1/gamingplatform.go
@@ -5,6 +5,8 @@ package connectfourv1
 const GameType = "ConnectFour.Game.v1"
 const GetGamesByPlayerType = "ConnectFour.GetGamesByPlayer.v1"
 const GetGamesByPlayerResponseType = "ConnectFour.GetGamesByPlayerResponse.v1"
+const GetOpenGamesType = "ConnectFour.GetOpenGames.v1"
+const GetOpenGamesResponseType = "ConnectFour.GetOpenGamesResponse.v1"
 const JoinGameType = "ConnectFour.JoinGame.v1"
 const JoinGameResponseType = "ConnectFour.JoinGameResponse.v1"
 const MakeMoveType = "ConnectFour.MakeMove.v1"
